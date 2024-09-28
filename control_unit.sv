b1e3871bfc56879ebc574abbb91789d42eaa345c
kthomas488 <64520346+kthomas488@users.noreply.github.com>
2024-09-28 11:46:25 +0530

    Update control_unit.sv

--- control_unit.sv
+++ control_unit.sv
@@ -4,7 +4,7 @@ module control_unit(input wire [6:0] op,input wire [2:0]funct3,input wire funct7
 
   wire [2:0] ALU_op;
   Main_decoder main_decoder(.op(op),.pcsrc(pcsrc),.resultsrc(resultsrc),.memwrite(memwrite),.alusrc(alusrc),.immsrc(immsrc),.regwrite(regwrite),.ALU_op(ALU_op));
-  ALU_decoder alu_decoder(.op(op),.funct3(funct3),.funct7(funct7),.ALU_op(ALU_op),.alu_control(alu_control);
+  ALU_decoder alu_decoder(.op(op),.funct3(funct3),.funct7(funct7),.ALU_op(ALU_op),.alu_control(alu_control));
 
   endmodule
                           
